3730d280bb7df82a538c12930cc1e5aa6183aa0b
Molnár Attila <molnar.attila@szlgbp.hu>
2023-01-30 16:46:38 +0100

    regex nagy cserék

---
+++ input/regexcsere/csv2sql/kisoutput_csv2sql.sql
@@ -0,0 +1,6 @@
+INSERT INTO Tanulok (id, nev, matcsop, ancsop, nyelv2csop, nem, egyuttlakoszam, testverszam)
+	VALUES (1,'Avon Mór','alfa','4. Kis','német','F',4,1);
+INSERT INTO Tanulok (id, nev, matcsop, ancsop, nyelv2csop, nem, egyuttlakoszam, testverszam)
+	VALUES (2,'Bakt Ernõ','alfa','4. Kis','spanyol','F',4,1);
+INSERT INTO Tanulok (id, nev, matcsop, ancsop, nyelv2csop, nem, egyuttlakoszam, testverszam)
+	VALUES (3,'Bal Margó','alfa','4. Kis','spanyol','L',2,0);
